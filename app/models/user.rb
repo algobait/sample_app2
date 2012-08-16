41ca6094609366454d2bd52a4e205e2bfa82baee
irvin <irvinduke@gmail.com>
2012-08-16 12:30:51 -0400

    ch7 exercises done

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ attr_accessible :email, :name, :password, :password_confirmation
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   
 end
